9556021d83145e7642ef49d231f3614be70b2206
Jonathan Moore <jonm@duck.com>
2022-09-05 13:36:19 -0500

    Basic Imagga client proof of concept

---
+++ src/imagga_client.rs
@@ -0,0 +1,42 @@
+use std::env::var;
+
+use serde::Serialize;
+use ureq::{post, Error, get};
+
+pub fn getImaggaAuthorization() -> String {
+    match (var("IMAGGA_API_KEY"), var("IMAGGA_API_SECRET")) {
+        (Ok(key), Ok(secret)) => {
+            let auth = base64::encode::<String>(
+                format!("{key}:{secret}")
+            );
+            format!("Basic {auth}")
+        },
+        (_,_) => {
+            panic!("Missing API key/secret")
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct ImaggaRequest<'a> {
+    pub image_url: &'a str
+}
+
+pub fn getTagsForUrl(url: &str) {
+    let result = get("https://api.imagga.com/v2/tags")
+        .set("Authorization", &getImaggaAuthorization())
+        .query("image_url", url)
+        .call();
+
+    match result {
+        Ok(response) => {
+            print!("{}", response.into_string().expect("convert to string"))
+        },
+        Err(Error::Status(code, text)) => {
+            print!("error {code} : {text:#?}")
+        }, Err(err) => {
+            print!("other error: {err}")
+        }
+    }
+
+}
