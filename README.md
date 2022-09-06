5bfcaab829061da1bd594715fef1a186a785e693
Jonathan Moore <jonm@duck.com>
2022-09-06 15:19:36 -0500

    Add database connection pool to Axum and automatically run migrations

---
+++ README.md
@@ -0,0 +1,9 @@
+# Image API
+
+## Setup
+Make sure to set the `IMAGGA_API_KEY`, `IMAGGA_API_SECRET`, and `DATABASE_URL` environmental variables first.
+
+The `DATABASE_URL` environmental variable should look something like:
+```
+postgres://username:password@localhost/image-api
+```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use axum::{Router, routing::get};
+use std::env;
+
+use axum::{Router, routing::get, Extension};
+use imagga_client::get_tags_for_url;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::Database;
+use tower::ServiceBuilder;
+pub mod imagga_client;
+
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // Database setup
+    let database_url = env::var("DATABASE_URL").expect("Missing DATABASE_URL environmental variable (see README.md)");
+    let database_connection = Database::connect(database_url)
+        .await
+        .expect("Unable to connect to database");
+    Migrator::up(&database_connection, None)
+        .await.unwrap();
+    
+    // Route and extension (i.e. for database) setup    
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(
+            ServiceBuilder::new()
+                .layer(Extension(database_connection))
+        );
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
+
